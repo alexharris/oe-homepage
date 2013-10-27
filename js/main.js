0ea4463c09a25bd1d774618193de5964889a04a5
Alex Harris <alexharris6@gmail.com>
2013-10-26 20:01:51 -0700

    add basic functionality

--- js/main.js
+++ js/main.js
@@ -1,1 +1,60 @@
+google.load('search', '1');
+
+var imageSearch;
+
+function searchComplete() {
+
+  // Check that we got results
+  if (imageSearch.results && imageSearch.results.length > 0) {
+    console.log(imageSearch.results.length);
+    // Grab our content div, clear it.
+    var contentDiv = document.getElementById('content');
+    contentDiv.innerHTML = '';
+
+    // Loop through our results, printing them to the page.
+    var results = imageSearch.results;
+    for (var i = 0; i < results.length; i++) {
+      // For each result write it's title and image to the screen
+      var result = results[i];
+      var imgContainer = document.createElement('div');
+
+      var newImg = document.createElement('img');
+      newImg.className = 'image';
+
+      // There is also a result.url property which has the escaped version
+      newImg.src=result.tbUrl;
+      imgContainer.appendChild(newImg);
+
+      // Put our title + image in the content
+      contentDiv.appendChild(imgContainer);
+    }
+
+  }
+}
+
+function OnLoad(currentVal) {
+
+  // Create an Image Search instance.
+  imageSearch = new google.search.ImageSearch();
+
+  // Set searchComplete as the callback function when a search is 
+  // complete.  The imageSearch object will have results in it.
+  imageSearch.setSearchCompleteCallback(this, searchComplete, null);
+
+  // Find me a beautiful car.
+  imageSearch.execute(currentVal);
+  
+  // Include the required Google branding
+  // google.search.Search.getBranding('branding');
+}
+
+function formChange() {
+  $('#optionsForm').change(function(){
+    currentVal = $( "input:radio[name=optionsRadios]:checked" ).val()
+    OnLoad(currentVal);
+
+  });
+}
+
+google.setOnLoadCallback(formChange);
 
